11be2fcfa1e3cf9b7f409c3e89a3d49e93cb5b67
Daniel Gustavo Leguizamon Rozo <daniel.leguizamon@mdcloudps.com>
2019-08-21 21:31:24 -0500

    add new initial network

--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/di/NetworkModule.kt
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/di/NetworkModule.kt
@@ -1,41 +1,9 @@
 package co.com.ceiba.mobile.pruebadeingreso.di
 
+import co.com.ceiba.mobile.pruebadeingreso.core.Network
 import co.com.ceiba.mobile.pruebadeingreso.data.remote.api.ApiService
-import co.com.ceiba.mobile.pruebadeingreso.rest.Endpoints.URL_BASE
-import okhttp3.OkHttpClient
 import org.koin.dsl.module
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
-import java.util.concurrent.TimeUnit
-import com.squareup.moshi.Moshi
-
-
-
-private const val CONNECT_TIMEOUT = 15L
-private const val WRITE_TIMEOUT = 15L
-private const val READ_TIMEOUT = 15L
 
 val networkModule = module {
-
-    val moshi = Moshi.Builder().build()
-
-    single {
-        OkHttpClient.Builder().apply {
-            cache(get())
-            connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
-            writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
-            readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
-            retryOnConnectionFailure(true)
-        }.build()
-    }
-
-    single {
-        Retrofit.Builder()
-                .baseUrl(URL_BASE)
-                .addConverterFactory(MoshiConverterFactory.create(moshi))
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build()
-                .create(ApiService::class.java)
-    }
+    single { Network().createWebService().create(ApiService::class.java) }
 }
