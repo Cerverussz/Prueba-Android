bdf1fca8eefac52e2ee37d57457f573727225ead
Daniel Leguizamon <dleguizamon@merqueo.com>
2020-02-06 22:44:49 -0500

    UI test modify

--- app/src/androidTest/java/co/com/ceiba/mobile/pruebadeingreso/MainActivityTest.kt
+++ app/src/androidTest/java/co/com/ceiba/mobile/pruebadeingreso/MainActivityTest.kt
@@ -1,13 +1,13 @@
 package co.com.ceiba.mobile.pruebadeingreso
 
+import android.content.Intent
 import android.view.View
 import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
-import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.RootMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -15,6 +15,7 @@ import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
 import co.com.ceiba.mobile.pruebadeingreso.view.activities.MainActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers
 import org.junit.Before
@@ -28,10 +29,11 @@ import org.junit.runner.RunWith
 class MainActivityTest {
 
     private lateinit var stringToBeTyped: String
-    private lateinit var textButton: String
 
     @get:Rule
     var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+    var activity: MainActivity = activityRule.launchActivity(Intent())
 
     private fun sleep(time: Long) = apply {
         Thread.sleep(time)
@@ -61,7 +63,30 @@ class MainActivityTest {
     @Before
     fun initValidString() {
         stringToBeTyped = "Ervin"
-        textButton = "Ver publicaciones"
+    }
+
+    @Test
+    fun scrollRecycler() {
+        sleep(1000)
+
+        onView(withId(R.id.recyclerViewSearchResults))
+            .perform(activity.recyclerViewSearchResults.adapter?.itemCount?.minus(
+                1
+            )?.let { RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(it) })
+
+        sleep(1000)
+
+        onView(withId(R.id.recyclerViewSearchResults))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(8, scrollTo()), closeSoftKeyboard())
+
+        sleep(500)
+        onView(withId(R.id.recyclerViewSearchResults))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, scrollTo()), closeSoftKeyboard())
+
+        sleep(500)
+        onView(withId(R.id.recyclerViewSearchResults))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(8, scrollTo()), closeSoftKeyboard())
+        sleep(3000)
     }
 
     @Test
